1c249573aa8dc38ac7f62077481a8d0c0aee4ce4
douyacun <douyacun@gmail.com>
2021-06-06 23:13:20 +0800

    f

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import easyocr
 from docx import Document
 from docx.shared import Pt
 from docx.oxml.ns import qn
+from docx.shared import Inches
 
 
 def print_hi():
@@ -10,7 +11,8 @@ def print_hi():
     doc.styles['Normal'].font.name = u'宋体'
     doc.styles['Normal']._element.rPr.rFonts.set(qn('w:eastAsia'), u'宋体')
     doc.styles['Normal'].font.size = Pt(12)
-    parag = doc.add_paragraph("")
+    parag = doc.add_paragraph("这部分内容根据在泡泡机器入的四期推送中正文前的絮叨以及与小伙伴们的互动整理而来")
+    parag.paragraph_format.first_line_indent = Pt(24)
 
     # font_styles = doc.styles
     # font_charstyle = font_styles.add_style('CommentsStyle', WD_STYLE_TYPE.CHARACTER)
@@ -20,8 +22,9 @@ def print_hi():
 
     # print(font_object.name)
 
-    parag.add_run("this word document, was created using Times New Roman").bold = True
-    run = parag.add_run("Python")
+    # parag.add_run("this word document, was created using Times New Roman").bold = True
+    # run = parag.add_run("Python")
+
     # print(run._element.rPr)
 
     doc.save("test.docx")
@@ -44,13 +47,16 @@ def detect_text(filepath: str):
     doc.styles['Normal']._element.rPr.rFonts.set(qn('w:eastAsia'), u'宋体')
     doc.styles['Normal'].font.size = Pt(12)
 
-    parag = doc.add_paragraph()
+    # parag = doc.add_paragraph()
 
     lines = merge_line(result)
     print(merge_line(result))
 
     for line in lines:
-        parag.add_run(line).add_break()
+        # parag.add_run(line).add_break()
+        p = doc.add_paragraph(line["text"])
+        if line["firstLineIndent"]:
+            p.paragraph_format.first_line_indent = Pt(24)
 
     doc.save("demo.docx")
 
@@ -76,41 +82,46 @@ def merge_line(res):
     3. 首行缩进？居中？
     """
     lineList = list()
-    line = ""
     border = parse_border(res)
     print(f"border: {border}")
 
+    line = {"text": "", "firstLineIndent": False}
     for i in range(len(res)):
         (bbox, text, prob) = res[i]
         pos = parse_pos(bbox)
+        print(f"pos: {pos}")
         prePos = parse_pos(res[i - 1][0])
-        print(f"text: {text} pos: {pos}  prePos: {prePos}")
         if i == 0:
-            line = text
+            line["text"] = text
         else:
             # 在图片中可以确认是统一行
             if pos["topY"] <= prePos["topY"] + 2 or pos["bottomY"] <= prePos["bottomY"] + 2:
-                line += text
+                line["text"] += text
             else:
                 # 确定是否需要换行
                 if prePos["rightX"] >= border["right"]:
-                    line += text
+                    line["text"] += text
                 else:
+                    # 开头
                     lineList.append(line)
-                    line = text
-    if line != "":
+                    line = {"text": text, "firstLineIndent": False}
+                    if pos["leftX"] > border["left"]:
+                        line["firstLineIndent"] = True
+    if line["text"] != "":
         lineList.append(line)
 
+    # print(lineList)
     return lineList
 
 
 def parse_pos(bbox) -> dict:
     (top_left, top_right, bottom_right, bottom_left) = bbox
-    res = dict()
-    res["topY"] = min(bottom_left[1], top_left[1])
-    res["bottomY"] = min(bottom_left[1], bottom_right[1])
-    res["leftX"] = min(top_left[0], bottom_left[0])
-    res["rightX"] = min(top_right[0], bottom_right[0])
+    res = {
+        "topY": min(bottom_left[1], top_left[1]),
+        "bottomY": min(bottom_left[1], bottom_right[1]),
+        "leftX": min(top_left[0], bottom_left[0]),
+        "rightX": min(top_right[0], bottom_right[0]),
+    }
     res["width"] = res["rightX"] - res["leftX"]
     res["height"] = res["bottomY"] - res["topY"]
     return res
@@ -139,7 +150,7 @@ def parse_border(res):
 
 if __name__ == '__main__':
     # print_hi()
-    # detect_text("/Users/admin/Desktop/c.png")
-    debug("/Users/admin/Desktop/c.png")
+    detect_text("/Users/admin/Desktop/c.png")
+    # debug("/Users/admin/Desktop/c.png")
     # debug("/Users/liuning/Desktop/d.png")
     # font_size(([30, 29], [521, 29], [521, 107], [30, 107]))
