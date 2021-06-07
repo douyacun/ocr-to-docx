eb601d30ba6dd40ff717fdb57512d5e84cdfa524
douyacun <douyacun@gmail.com>
2021-06-07 18:44:34 +0800

    fuck

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import easyocr
 from docx import Document
 from docx.shared import Pt
 from docx.oxml.ns import qn
-from docx.shared import Inches
+import math
+
+DIFF = 2
+BASE_FONT_SIZE = 12
 
 
 def print_hi():
@@ -10,9 +13,9 @@ def print_hi():
 
     doc.styles['Normal'].font.name = u'宋体'
     doc.styles['Normal']._element.rPr.rFonts.set(qn('w:eastAsia'), u'宋体')
-    doc.styles['Normal'].font.size = Pt(12)
+    doc.styles['Normal'].font.size = Pt(BASE_FONT_SIZE)
     parag = doc.add_paragraph("这部分内容根据在泡泡机器入的四期推送中正文前的絮叨以及与小伙伴们的互动整理而来")
-    parag.paragraph_format.first_line_indent = Pt(24)
+    parag.paragraph_format.first_line_indent = Pt(BASE_FONT_SIZE * 2)
 
     # font_styles = doc.styles
     # font_charstyle = font_styles.add_style('CommentsStyle', WD_STYLE_TYPE.CHARACTER)
@@ -45,18 +48,19 @@ def detect_text(filepath: str):
 
     doc.styles['Normal'].font.name = u'宋体'
     doc.styles['Normal']._element.rPr.rFonts.set(qn('w:eastAsia'), u'宋体')
-    doc.styles['Normal'].font.size = Pt(12)
+    doc.styles['Normal'].font.size = Pt(BASE_FONT_SIZE)
 
     # parag = doc.add_paragraph()
 
     lines = merge_line(result)
-    print(merge_line(result))
+    print(lines)
 
     for line in lines:
         # parag.add_run(line).add_break()
-        p = doc.add_paragraph(line["text"])
+        p = doc.add_paragraph()
         if line["firstLineIndent"]:
-            p.paragraph_format.first_line_indent = Pt(24)
+            p.paragraph_format.first_line_indent = Pt(BASE_FONT_SIZE * 2)
+        p.add_run(line["text"]).font.size = Pt(line["fontSize"])
 
     doc.save("demo.docx")
 
@@ -84,8 +88,10 @@ def merge_line(res):
     lineList = list()
     border = parse_border(res)
     print(f"border: {border}")
+    fontList = parse_fontsize(res)
+    print(f"font: {fontList}")
 
-    line = {"text": "", "firstLineIndent": False}
+    line = {"text": "", "firstLineIndent": False, "fontSize": BASE_FONT_SIZE}
     for i in range(len(res)):
         (bbox, text, prob) = res[i]
         pos = parse_pos(bbox)
@@ -93,18 +99,21 @@ def merge_line(res):
         prePos = parse_pos(res[i - 1][0])
         if i == 0:
             line["text"] = text
+            line["fontSize"] = get_fontsize(fontList, pos["height"])
+            if pos["leftX"] > border["left"]:
+                line["firstLineIndent"] = True
         else:
             # 在图片中可以确认是统一行
             if pos["topY"] <= prePos["topY"] + 2 or pos["bottomY"] <= prePos["bottomY"] + 2:
                 line["text"] += text
             else:
                 # 确定是否需要换行
-                if prePos["rightX"] >= border["right"]:
+                if prePos["rightX"] >= border["right"] and pos["leftX"] <= border["left"]:
                     line["text"] += text
                 else:
                     # 开头
                     lineList.append(line)
-                    line = {"text": text, "firstLineIndent": False}
+                    line = {"text": text, "firstLineIndent": False, "fontSize": get_fontsize(fontList, pos["height"])}
                     if pos["leftX"] > border["left"]:
                         line["firstLineIndent"] = True
     if line["text"] != "":
@@ -112,6 +121,40 @@ def merge_line(res):
 
     # print(lineList)
     return lineList
+
+
+def get_fontsize(fontList: list, height: int):
+    for v in fontList:
+        if v["minHeight"] <= height <= v["maxHeight"]:
+            return v["fontSize"]
+    return BASE_FONT_SIZE
+
+
+# 预估字体大小
+def parse_fontsize(res) -> list:
+    fontList = []
+    heightDict = {}
+    for i in range(len(res)):
+        (bbox, text, prob) = res[i]
+        pos = parse_pos(bbox)
+        heightDict[pos["height"]] = 1
+
+    minHeight = 0
+    for k in sorted(heightDict.keys()):
+        # 初始值
+        if minHeight == 0:
+            minHeight = k
+            font = {"minHeight": k, "maxHeight": k + 2, "fontSize": BASE_FONT_SIZE}
+            fontList.append(font)
+            continue
+
+        if k > minHeight + DIFF:
+            minHeight = k
+            preFont = fontList[-1]
+            font = {"minHeight": k, "maxHeight": k + 2,
+                    "fontSize": math.floor(k / preFont["minHeight"] * BASE_FONT_SIZE)}
+            fontList.append(font)
+    return fontList
 
 
 def parse_pos(bbox) -> dict:
@@ -143,14 +186,16 @@ def parse_border(res):
         right = max(right, top_right[0], bottom_right[0])
         left = min(left, top_left[0], bottom_left[0])
 
-    border["left"] = left - 2
-    border["right"] = right - 2
+    border["left"] = left - DIFF
+    border["right"] = right - DIFF
     return border
 
 
 if __name__ == '__main__':
     # print_hi()
-    detect_text("/Users/admin/Desktop/c.png")
+    # detect_text("/Users/admin/Desktop/c.png")
+    # detect_text("/Users/liuning/Desktop/c.png")
+    detect_text("/Users/liuning/Desktop/d.png")
     # debug("/Users/admin/Desktop/c.png")
     # debug("/Users/liuning/Desktop/d.png")
     # font_size(([30, 29], [521, 29], [521, 107], [30, 107]))
